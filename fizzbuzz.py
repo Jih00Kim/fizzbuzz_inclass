6df4698b7db989feefc7fb067828d553fa238ace
Jih00Kim <jihoo1986@gmail.com>
2021-03-04 13:09:00 +0900

    feat: completd code

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,1 +1,4 @@
-print(['fizzbuzz' if i%15==0  else 'fizz' if i%3 ==0 else 'buzz' if i%5 ==0 else i for i in range(1,100+1)]) 
+def print_(obj):
+	print(obj,end='\t')
+fb_list = ['fizzbuzz' if i%15==0  else 'fizz' if i%3 ==0 else 'buzz' if i%5 ==0 else i for i in range(1,100+1)])
+print(fb_list) 
