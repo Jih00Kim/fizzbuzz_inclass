a6dc5a0d3b728d5c2523138a8ab41d0cd49d3f59
Jih00Kim <jihoo1986@gmail.com>
2021-03-04 13:55:53 +0900

    feat: completed game.py
    but the answer appears before guess

--- numguess/game.py
+++ numguess/game.py
@@ -3,4 +3,18 @@ user_name = input('Enter your name:')
 answer = random.randint(1,100)
 
 # for debugging
-print(user_name, answer)
+print(answer)
+
+guess = int(input('welcome, {}. Guess the nimber! :'.format(user_name)))
+
+#for debugging.
+print(answer,guess,type(guess))
+
+if guess == answer:
+	print('correct!')
+else:
+	print('Wrong! The answer is {}.'.format(answer))
+
+
+
+
