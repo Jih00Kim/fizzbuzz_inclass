c1cdfbc63fdafb3bd6c5df8051e7892313457547
Jih00Kim <jihoo1986@gmail.com>
2021-03-04 11:08:45 +0900

    factor: changed all output in korean

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,12 +2,12 @@ def print_(obj):
 	print(obj, end='\t')
 for i in range(1,101):
 	if i % 15 == 0:
-		print_('fizzbuzz')
+		print_('피즈버즈')
 	else:
 		if i % 3 == 0:
-			print_('fizz')
+			print_('피즈')
 		elif i % 5 == 0:
-			print_('buzz')
+			print_('버즈')
 		else:
 			print_(i)
 
