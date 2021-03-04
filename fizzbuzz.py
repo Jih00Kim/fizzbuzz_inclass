80b8aa23621242936335618ea16389d33c8d175e
Jih00Kim <jihoo1986@gmail.com>
2021-03-04 10:29:35 +0900

    feat: modify fizzbuzz.py on new branch
    branch name fizzbuzz-if

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,4 +1,12 @@
 for i in range(1,101):
-	print(i, end = '\t')
+	if i % 15 == 0:
+		print('fizzbuzz')
+	else:
+		if i % 3 == 0:
+			print('fizz')
+		elif i % 5 == 0:
+			print('buzz')
+		else:
+			print(i)
 
 print('') 
